19f2c1c68b9a0d14de0f82caaf64f7085a1b2a1c
Marco Ramirez <marco.rmz@live.com>
2020-01-30 10:06:16 -0800

    - Add sandbox APIKEY from iexapis - Bugfix variable error template in chart component

--- apps/stocks/src/environments/environment.ts
+++ apps/stocks/src/environments/environment.ts
@@ -6,7 +6,7 @@ import { StocksAppConfig } from '@coding-challenge/stocks/data-access-app-config
 
 export const environment: StocksAppConfig = {
   production: false,
-  apiKey: '',
+  apiKey: 'Tpk_8807c30456f84f3d86600ca878b15f7a',
   apiURL: 'https://sandbox.iexapis.com'
 };
 
