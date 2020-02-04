d5cdabf9a6454f3cc3d7d8d89ff9dc56a3dcd03d
marco-rmz <marco.rmz@live.com>
2020-02-04 08:53:11 -0800

    - Unsubscribe to the form value changes

--- libs/stocks/feature-shell/src/lib/stocks/stocks.component.ts
+++ libs/stocks/feature-shell/src/lib/stocks/stocks.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PriceQueryFacade } from '@coding-challenge/stocks/data-access-price-query';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'coding-challenge-stocks',
   templateUrl: './stocks.component.html',
   styleUrls: ['./stocks.component.css']
 })
-export class StocksComponent implements OnInit {
+export class StocksComponent implements OnInit, OnDestroy {
   stockPickerForm: FormGroup;
   symbol: string;
   period: string;
+  private subs: Subscription
 
   quotes$ = this.priceQuery.priceQueries$;
 
@@ -33,7 +35,7 @@ export class StocksComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.stockPickerForm.valueChanges.subscribe(x => this.fetchQuote(this));
+    this.subs = this.stockPickerForm.valueChanges.subscribe(x => this.fetchQuote(this));
   }
 
   fetchQuote(value) {
@@ -41,5 +43,9 @@ export class StocksComponent implements OnInit {
       const { symbol, period } = this.stockPickerForm.value;
       this.priceQuery.fetchQuote(symbol, period);
     }
+  }
+
+  ngOnDestroy(){
+    this.subs.unsubscribe();
   }
 }
